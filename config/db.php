ffcab27807b2c8af93b262d239b29d05c1491bcd
pichai2514 <pichai2514@users.noreply.github.com>
2016-02-18 09:04:54 +0700

    Update db.php
    เปลี่ยนฐานข้อมูล

--- config/db.php
+++ config/db.php
@@ -2,7 +2,7 @@
 
 return [
     'class' => 'yii\db\Connection',
-    'dsn' => 'mysql:host=localhost:3310;dbname=gov_book1',
+    'dsn' => 'mysql:host=localhost:3310;dbname=gov_book59',
     'username' => 'root',
     'password' => 'root',
     'charset' => 'utf8',
